42607fafc0aa4883bbd6c03c6211943887324ec8
khan9920 <shafkhanmohamed23@gmail.com>
2020-05-21 20:56:56 +0530

    Contact us completed

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ export default class Contact extends Component {
                 <Title title="CONTACT" />
                 <div className="row row-contact">
                     <div className="col-md-6">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.3479599298603!2d79.97074693496496!3d6.91468433909214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae256db1a6771c5%3A0x2c63e344ab9a7536!2sSri%20Lanka%20Institute%20of%20Information%20Technology!5e0!3m2!1sen!2slk!4v1590073807479!5m2!1sen!2slk" width="600" height="450" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.3479599298603!2d79.97074693496496!3d6.91468433909214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae256db1a6771c5%3A0x2c63e344ab9a7536!2sSri%20Lanka%20Institute%20of%20Information%20Technology!5e0!3m2!1sen!2slk!4v1590073807479!5m2!1sen!2slk" width="600" height="450" frameborder="0" aria-hidden="false" tabindex="0"></iframe>
                     </div>
                     <div className="col-md-6">
                         <div class="wrapper-contact-details">
