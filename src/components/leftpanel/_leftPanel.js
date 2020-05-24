a015613a26047bef03bdb7f5782f8e1a70fd5db9
Charith <charith.rhettiarachchi@gmail.com>
2020-05-24 18:16:18 +0530

    left panel update

--- src/components/leftpanel/_leftPanel.js
+++ src/components/leftpanel/_leftPanel.js
@@ -9,7 +9,6 @@ export default class _leftPanel extends Component {
                 <ul>
                     <li><Link to='/store/admin/products'><i className="pi pi-tags mr-2"/>PRODUCTS</Link></li>
                     <li><Link to='/store/admin/categories'><i className="pi pi-list mr-2"/>CATEGORIES</Link></li>
-                    <li><Link to='/store/admin/orders'><i className="pi pi-dollar mr-2"/>ORDERS</Link></li>
                     <li><Link to='/store/admin/users'><i className="pi pi-user mr-2"/>USERS</Link></li>
                 </ul>
             </div>
